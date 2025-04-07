df6e9e0b8e13407e39d5d3385e026308af58efe3
HollosJ <hollosj99@gmail.com>
2025-04-08 00:07:48 +0100

    Add prettier with astro and tailwind plugins. Write to astro-app subrepo.

--- astro-app/astro.config.mjs
+++ astro-app/astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import sanity from '@sanity/astro';
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,8 +13,8 @@ export default defineConfig({
 
   integrations: [
     sanity({
-      projectId: '<project-id>',
-      dataset: 'production',
+      projectId: "<project-id>",
+      dataset: "production",
       useCdn: false, // false if building statically
     }),
   ],
--- astro-app/sanity.js
+++ astro-app/sanity.js
@@ -1,11 +1,11 @@
-import { createClient } from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: '<project-id>',
-  dataset: 'production',
+  projectId: "<project-id>",
+  dataset: "production",
   useCdn: false,
-  apiVersion: '2025-01-01',
+  apiVersion: "2025-01-01",
 });
 
 const builder = imageUrlBuilder(client);
